4044ec4df4743de0b54cd56ba6392ceffb4e3df3
acram002 <cramer_alex@yahoo.com>
2026-07-19 01:43:11 +0000

    Removed duplicate file, created outline for server hardening

--- bash/server-hardening/server-hardening.sh
+++ bash/server-hardening/server-hardening.sh
@@ -1,4 +1,49 @@
 #!/usr/bin/env bash
 
+check_sshd_config(){
+if
+	change_sshd_config()
+else
+	echo "ssh config secure"
+fi
+}
+
+set_fw() {
+
+echo "fw set, only allow port 22 for ansible control node"
+}
+
+config_updates() {
+apt_update()
+
+# config unattend sec updates
+echo "system updated, automatic security updates config"
+}
+
+basic_hardening() {
+# install & config Fail2Ban for brute-force ssh
+
+echo "Fail2Ban installed and config for ssh attacks"
+}
+
+server_config() {
+# set correct timezonel; ntp?
+# set hostname
+
+echo "correct time and hostname set"
+}
+
+check_svc_mgmt() {
+# test systemctl services, start/stop, enable at boot
+
+echo "systemctl svcs working and configured"
+}
+
+sec_checklist() {
+
+echo "all tests passed. linux server hardeninig complete"
+}
+
+
 read -s -p "enter pw" $pw
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
